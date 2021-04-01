56599ab23d222b217c9b932853f740e41db5b88a
khadija <khadijalahmar@gmail.com>
2021-04-01 19:57:05 +0100

    select cartes et lien de projet

---
+++ Breif/Etudiant/notification.php
@@ -0,0 +1,51 @@
+<?php
+include_once('conexion.php');
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/notification.css">
+    <title>notification</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+                    <div class="logo">
+                        <h1 class="you">You </h1>
+                        <h1 class="code">Code </h1>
+                    </div>
+                    <div class="para_noti">
+                     <a href="parametre.php"> <img src="../Conception_Frontend/imag/Parametre.png" alt="setting" class="img2"></a>
+                    </div>
+        </div>
+        <div class="notif">
+            <h1>titre</h1>
+            <div class="format">
+                <h3>Formateur</h3>
+                <h3>12/09/2021 a 13:14</h3>
+            </div>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius, pariatur autem? Nihil fugit veritatis illum, ducimus, porro quidem recusandae dignissimos odio maxime commodi molestiae reprehenderit officiis ipsum est totam beatae!
+            </p>
+        </div>
+        <div class="notif">
+            <h1>titre</h1>
+            <div class="format">
+                <h3>Formateur</h3>
+                <h3>12/09/2021 a 13:14</h3>
+            </div>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius, pariatur autem? Nihil fugit veritatis illum, ducimus, porro quidem recusandae dignissimos odio maxime commodi molestiae reprehenderit officiis ipsum est totam beatae!
+            </p>
+        </div>
+        <div class="footer">
+            <p>©</p>
+        </div>
+    </div>
+</body>
+</html>
